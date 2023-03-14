b225ea9299fa27583d8b317513d41b68a1bff441
Alisa <alisa.luoma@hotmail.fi>
2023-03-14 09:03:51 +0200

    Base project

---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import logo from '../../Images/react.png';
+
+const Header = () => {
+  return(
+    <div className="col-12 py-3 pl-2" style={{borderBottom: "1px solid #7777"}}>
+      <img src={logo} style={{height: "35px", verticalAlign: "top"}} alt="React logo"></img>
+      <span className="h2 pt-4 m-2 text-white-50">WatchOPedia</span>
+    </div>
+  )
+}
+
+export default Header;
